04ad6418b8977cee28dff6e5b0b04cc71e747870
David Pierron <dpierron@gmail.com>
2025-09-20 16:29:59 +0200

    - added todo - silenced ATC

--- Modern/VEAF_MA_loader.lua
+++ Modern/VEAF_MA_loader.lua
@@ -1,33 +1,4 @@
-local MA_DYNAMIC_SCRIPTS_PATH = MA_DYNAMIC_PATH .. [[..\scripts\]]
-
--- setup first batch of configuration (no need for any script before this one)
 Era = 'Modern' -- Era can be 'Coldwar' or 'Modern'
-UseStatics=true
-CreditLosewhenKilled=false -- if true, Blue coalition will lose 100 points for every player death
-NoSA10AndSA11=false
-SplashDamage=false
-ShowKills=true -- if true, a messagw will come up upon kills
-StoreLimit = false
-CTLDCost=true -- if the above is false, then ctld stuff wil not cost anything. default is true
 
--- load first batch of scripts
-assert(loadfile(MA_DYNAMIC_SCRIPTS_PATH .. "Moose_2025-08-14.lua")) ()
-assert(loadfile(MA_DYNAMIC_SCRIPTS_PATH .. "Hercules_Cargo.lua")) ()
-assert(loadfile(MA_DYNAMIC_SCRIPTS_PATH .. "zoneCommander_moose.lua")) ()
-
--- setup second batch of configuration (need to load zoneCommander_moose.lua script before)
-GlobalSettings.setDifficultyScaling(1.4,1) -- red respawn factor
-GlobalSettings.setDifficultyScaling(1.1,2) -- blue respawn factor
-
--- load the rest of the scripts
-assert(loadfile(MA_DYNAMIC_SCRIPTS_PATH .. "MA_Setup_CWG.lua")) ()
-assert(loadfile(MA_DYNAMIC_SCRIPTS_PATH .. "WelcomeMessage_CWG.lua")) ()
-assert(loadfile(MA_DYNAMIC_SCRIPTS_PATH .. "Zeus.lua")) ()
-assert(loadfile(MA_DYNAMIC_SCRIPTS_PATH .. "weaponslist.lua")) ()
-assert(loadfile(MA_DYNAMIC_SCRIPTS_PATH .. "Foothold_CWG_MANTIS.lua")) ()
---assert(loadfile(MA_DYNAMIC_SCRIPTS_PATH .. "noFlyZone Caucasus.lua")) ()
-assert(loadfile(MA_DYNAMIC_SCRIPTS_PATH .. "EWRS.lua")) ()
-assert(loadfile(MA_DYNAMIC_SCRIPTS_PATH .. "MA_CTLD_CWG.lua")) ()
-assert(loadfile(MA_DYNAMIC_SCRIPTS_PATH .. "Splash_Damage_3.4.1_leka.lua")) ()
-assert(loadfile(MA_DYNAMIC_SCRIPTS_PATH .. "AIEN.lua")) ()
-
+-- load the common configuration file
+assert(loadfile(MA_DYNAMIC_PATH .. [[..\scripts\VEAF_common.lua]])) ()
--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # VEAF Foothold Caucasus
 
-This is a modification of the original [Foothold Germany](https://www.digitalcombatsimulator.com/en/files/3345164/) mission by Lekaa, that I adapted to make it load all the script files and config direclty from the server.
+## Presentation
+
+This is a modification of the original [Foothold Germany](https://www.digitalcombatsimulator.com/en/files/3345164/) mission by Lekaa, that I adapted to make it load all the script files and config directly from the server.
 
 **Done using the latest version updated on 2025-08-11.**
+
+## Game master slots
+
+They are protected for veaf Members Only
+
+## Things to do
+
+- [x] silence all ATC
+- [ ] add VEAF radio presets
